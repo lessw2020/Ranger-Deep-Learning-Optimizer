3a3ac7d36068d50bbe150a239d881b539cad3cd8
Less Wright <lessw@etrillium.com>
2019-08-18 22:24:23 -0700

    Add files via upload

---
+++ ranger.py
@@ -0,0 +1,118 @@
+
+import math
+import torch
+from torch.optim.optimizer import Optimizer, required
+import itertools as it
+#from torch.optim import Optimizer
+#credit - Lookahead implementation from LonePatient - https://github.com/lonePatient/lookahead_pytorch/blob/master/optimizer.py
+#credit2 - RAdam code by https://github.com/LiyuanLucasLiu/RAdam/blob/master/radam.py
+
+
+class Ranger(Optimizer):
+    
+    def __init__(self, params, lr=1e-3, alpha=0.5, k=6, betas=(.9,0.999), eps=1e-8, weight_decay=0):
+        if not 0.0 <= alpha <= 1.0:
+            raise ValueError(f'Invalid slow update rate: {alpha}')
+        if not 1 <= k:
+            raise ValueError(f'Invalid lookahead steps: {k}')
+        
+        for group in self.param_groups:
+            group["step_counter"] = 0
+            print("group step counter init")
+                      
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        self.alpha = alpha
+        self.k = k        
+        self.radam_buffer = [[None,None,None] for ind in range(10)]
+        
+        super(Ranger, self).__init__(params,defaults)
+        
+        
+        self.slow_weights = [[p.clone().detach() for p in group['params']]
+                                for group in self.param_groups]
+
+        for w in it.chain(*self.slow_weights):
+            w.requires_grad = False
+        
+    def __setstate__(self, state):
+        print("set state called")
+        super(Ranger, self).__setstate__(state)
+       
+        
+    def step(self, closure=None):
+        loss = None
+        #if closure is not None:
+            #loss = closure()
+        #------------ radam
+        for group in self.param_groups:
+    
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data.float()
+                if grad.is_sparse:
+                    raise RuntimeError('RAdam does not support sparse gradients')
+    
+                p_data_fp32 = p.data.float()
+    
+                state = self.state[p]
+    
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['exp_avg'] = torch.zeros_like(p_data_fp32)
+                    state['exp_avg_sq'] = torch.zeros_like(p_data_fp32)
+                else:
+                    state['exp_avg'] = state['exp_avg'].type_as(p_data_fp32)
+                    state['exp_avg_sq'] = state['exp_avg_sq'].type_as(p_data_fp32)
+    
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+    
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+    
+                state['step'] += 1
+                buffered = self.radam_buffer[int(state['step'] % 10)]
+                if state['step'] == buffered[0]:
+                    N_sma, step_size = buffered[1], buffered[2]
+                else:
+                    buffered[0] = state['step']
+                    beta2_t = beta2 ** state['step']
+                    N_sma_max = 2 / (1 - beta2) - 1
+                    N_sma = N_sma_max - 2 * state['step'] * beta2_t / (1 - beta2_t)
+                    buffered[1] = N_sma
+                    if N_sma > 5:
+                        step_size = group['lr'] * math.sqrt((1 - beta2_t) * (N_sma - 4) / (N_sma_max - 4) * (N_sma - 2) / N_sma * N_sma_max / (N_sma_max - 2)) / (1 - beta1 ** state['step'])
+                    else:
+                        step_size = group['lr'] / (1 - beta1 ** state['step'])
+                    buffered[2] = step_size
+    
+                if group['weight_decay'] != 0:
+                    p_data_fp32.add_(-group['weight_decay'] * group['lr'], p_data_fp32)
+    
+                if N_sma > 5:                    
+                    denom = exp_avg_sq.sqrt().add_(group['eps'])
+                    p_data_fp32.addcdiv_(-step_size, exp_avg, denom)
+                else:
+                    p_data_fp32.add_(-step_size, exp_avg)
+    
+                p.data.copy_(p_data_fp32)
+        
+        
+        #---------------- end radam step
+        
+        #look ahead tracking and updating if latest batch = k
+        for group,slow_weights in zip(self.param_groups,self.slow_weights):
+            group['step_counter'] += 1
+            if group['step_counter'] % self.k != 0:
+                continue
+            for p,q in zip(group['params'],slow_weights):
+                if p.grad is None:
+                    continue
+                q.data.add_(self.alpha,p.data - q.data)
+                p.data.copy_(q.data)
+            
+        
+            
+        return loss
+
