1391288922bb43a883720e486f577a2fa3409644
Scott Lowe <scott.code.lowe@gmail.com>
2020-04-15 10:21:59 +0100

    DOC: Add README.md as package long_description

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ import os
 from setuptools import find_packages, setup
 
 
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
 setup(
     name='ranger',
     version='0.0.1',
@@ -13,6 +18,8 @@ setup(
     package_dir={'ranger': os.path.join('.', 'ranger')},
     description='Ranger - a synergistic optimizer using RAdam '
                 '(Rectified Adam) and LookAhead in one codebase ',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     author='Less Wright',
     license='Apache',
     install_requires=['torch']
