d9513b1322316edb2eeac3627b65a5ea794f5258
Scott Lowe <scott.code.lowe@gmail.com>
2020-04-15 10:19:34 +0100

    BUG: Add package to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-from setuptools import setup
+#!/usr/bin/env python
+
+import os
+from setuptools import find_packages, setup
+
 
 setup(
     name='ranger',
     version='0.0.1',
+    packages=find_packages(
+        exclude=['tests', '*.tests', '*.tests.*', 'tests.*']
+    ),
+    package_dir={'ranger': os.path.join('.', 'ranger')},
     description='Ranger - a synergistic optimizer using RAdam '
                 '(Rectified Adam) and LookAhead in one codebase ',
     author='Less Wright',
